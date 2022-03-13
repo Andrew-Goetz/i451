e2172afdd4e0b9bf829dbf63731bc7ccb533c031
Andrew Goetz <agoetz3@binghamton.edu>
2022-03-12 21:17:52 -0500

    finished project 2

--- submit/prj2-sol/multiproc-knn-ocr/src/main.rs
+++ submit/prj2-sol/multiproc-knn-ocr/src/main.rs
@@ -2,11 +2,14 @@ use std::path::Path;
 use std::env;
 use std::error::Error;
 
-use knn_ocr::{read_labeled_data, knn, LabeledFeatures};
-use nix::unistd::{fork, ForkResult, pipe, close, Pid, read, write};
-use nix::sys::wait::waitpid;
 use std::os::unix::prelude::RawFd;
 use std::process::exit;
+
+use knn_ocr::{read_labeled_data, knn, LabeledFeatures};
+
+use nix::unistd::{fork, ForkResult, pipe, close, Pid, read, write, getppid};
+use nix::sys::wait::waitpid;
+use nix::sys::signal::{signal, kill, SIGTERM, SigHandler};
 
 const TEST_DATA: &str = "t10k-images-idx3-ubyte";
 const TEST_LABELS: &str  = "t10k-labels-idx1-ubyte";
@@ -55,24 +58,42 @@ impl Args {
     	Ok(Args { data_dir, k, n_test, n_proc })
     }
 }
-/*
-fn if_error(pids: Vec<Pid>) {
 
+static mut CHILD_PIDS: Vec<Pid> = Vec::new(); // necessary global to get this info to signal handler
+
+extern "C" fn kill_children(_x: i32) {
+    for i in 0..unsafe{CHILD_PIDS.len()} {
+        unsafe{kill(CHILD_PIDS[i], SIGTERM)}.expect(&format!("Failed to kill child with PID {}", unsafe{CHILD_PIDS[i]}));
+    }
+    exit(0); // kill self
 }
-*/
+
 fn do_child(train_data: &Vec<LabeledFeatures>, send_result: RawFd, get_data: RawFd, proc_id: usize, args: &Args) {
     let mut size: [u8; 8] = [0; 8];
-    read(get_data, &mut size).expect("Failed to read from pipe");
+        //.expect;
+    match read(get_data, &mut size) {
+        Ok(result) => result,
+        Err(err) => { 
+            kill(getppid(), SIGTERM).unwrap();
+            panic!("Error: {:?}", err);
+        }
+    };
     let n = usize::from_le_bytes(size[0..8].try_into().expect("Conversion failed in child"));
     let mut img_num = n / args.n_proc;
     if n % args.n_proc > proc_id { 
         img_num += 1;
     }
-    //println!("for proc{}: {}", proc_id, img_num);
+    println!("for proc{}: {}", proc_id, img_num);
     let mut send: [u8; 10] = [0; 10];
     let mut img: [u8; 785] = [0; 785];
     for _i in 0..img_num {
-        read(get_data, &mut img).expect("Failed to read from pipe");
+        match read(get_data, &mut img) {
+            Ok(result) => result,
+            Err(err) => { 
+                kill(getppid(), SIGTERM).unwrap();
+                panic!("Error: {:?}", err);
+            }
+        };
 	    let nearest_index = knn(&train_data, &img[1..].to_vec(), args.k);
 	    let predicted = train_data[nearest_index].label;
 	    let expected = img[0]; // img[0] is the label
@@ -82,7 +103,13 @@ fn do_child(train_data: &Vec<LabeledFeatures>, send_result: RawFd, get_data: Raw
         }
         send[8] = predicted;
         send[9] = expected;
-        write(send_result, &send).expect("Failed to write to pipe");
+        match write(send_result, &send) {
+            Ok(result) => result,
+            Err(err) => { 
+                kill(getppid(), SIGTERM).unwrap();
+                panic!("Error: {:?}", err);
+            }
+        };
     }
     close(send_result).expect("Failed to close file descriptor");
     close(get_data).expect("Failed to close file descriptor");
@@ -99,34 +126,28 @@ fn main() {
     let train_data = read_labeled_data(&args.data_dir, TRAINING_DATA, TRAINING_LABELS);
 
     // Spawn worker processes and their pipes, printing out (and recording) PIDs
-    let mut child_pids: Vec<Pid> = Vec::new();
     let mut c2p_pipes = Vec::new();
     let mut p2c_pipes = Vec::new();
+    let sig_handler = SigHandler::Handler(kill_children);
+    unsafe{signal(SIGTERM, sig_handler)}.expect("Signal failed");
     for i in 0..args.n_proc {
         c2p_pipes.push(pipe().expect("Failed to create pipe"));
         p2c_pipes.push(pipe().expect("Failed to create pipe"));
         match unsafe{fork()} {
             Ok(ForkResult::Parent{child}) => {
                 println!("Process with PID {} created", child);
-                child_pids.push(child);
+                unsafe{CHILD_PIDS.push(child)};
                 close(c2p_pipes[i].1).expect("Failed to close file descriptor");
                 close(p2c_pipes[i].0).expect("Failed to close file descriptor");
             }
             Ok(ForkResult::Child) => {
-                /*
-                for j in 0..i {
-                    close(c2p_pipes[j].0).expect("Failed to close file descriptor");
-                    close(c2p_pipes[j].1).expect("Failed to close file descriptor");
-                    close(p2c_pipes[j].0).expect("Failed to close file descriptor");
-                    close(p2c_pipes[j].1).expect("Failed to close file descriptor");
-                }
-                */
                 close(c2p_pipes[i].0).expect(&format!("failed to close fd for i={}", i));
                 close(p2c_pipes[i].1).expect("Failed to close file descriptor");
                 do_child(&train_data, c2p_pipes[i].1, p2c_pipes[i].0, i, &args);
             }
             Err(_) => {
-                panic!("Fork failed");
+                println!("Fork failed, killing children and terminating");
+                kill_children(16); // have kill any child process that may have already been created
             }
         }
     }
@@ -142,8 +163,14 @@ fn main() {
     let mut ok = 0;
     let mut buf: [u8; 10] = [0; 10];
     let mut img: [u8; 785] = [0; 785]; //index 0 = label, indexes 1..784 = image data
-    for i in 0..args.n_proc { // send size to each worker
-        write(p2c_pipes[i].1, &n.to_le_bytes()).expect("Failed to write to pipe");
+    for i in 0..args.n_proc { // send size to each worker first
+        match write(p2c_pipes[i].1, &n.to_le_bytes()) {
+            Ok(result) => result,
+            Err(err) => {
+                kill_children(15);
+                panic!("Error: {:?}", err);
+            }
+        };
     }
     for i in 0..n {
         img[0] = test_data[i].label;
@@ -151,8 +178,21 @@ fn main() {
             img[j] = test_data[i].features[j-1];
         }
         //println!("i%args.n_proc={}",i % args.n_proc);
-        write(p2c_pipes[i % args.n_proc].1, &img).expect("Failed to write to pipe");
-        read(c2p_pipes[i % args.n_proc].0, &mut buf).expect("Failed to read from pipe");
+        match write(p2c_pipes[i % args.n_proc].1, &img) {
+            Ok(result) => result,
+            Err(err) => {
+                kill_children(15);
+                panic!("Error: {:?}", err);
+            }
+        };
+        match read(c2p_pipes[i % args.n_proc].0, &mut buf) {
+            Ok(result) => result,
+            Err(err) => {
+                kill_children(15);
+                panic!("Error: {:?}", err);
+            }
+        };
+
         let nearest_index = usize::from_le_bytes(buf[0..8].try_into().expect("Conversion failed in parent"));
         let predicted = buf[8];
         let expected = buf[9];
@@ -173,7 +213,7 @@ fn main() {
     for i in 0..args.n_proc {
         close(c2p_pipes[i].0).expect("Failed to close file descriptor");
         close(p2c_pipes[i].1).expect("Failed to close file descriptor");
-        waitpid(child_pids[i], None).expect("waitpid() failed");
+        waitpid(unsafe{CHILD_PIDS[i]}, None).expect("waitpid() failed");
         //println!("Are we hanging here? i = {}", i);
     }
 }
