e9794a7646a5036fce5934beceb64206b118e095
Andrew Goetz <agoetz3@binghamton.edu>
2022-04-02 17:29:47 -0400

    I tired

--- submit/prj3-sol/knnc/main.c
+++ submit/prj3-sol/knnc/main.c
@@ -6,7 +6,30 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define KNOWN_FIFO "notify.fifo"
+#define KNOWN_FIFO "notify"
+
+void error_handle(void) {
+	perror("Error");
+	exit(EXIT_FAILURE);
+}
+
+void notify_daemon(char *SERVER_DIR, char pid[20]) {
+	char *fifo = strcat(SERVER_DIR, KNOWN_FIFO);
+	int fd = open(fifo, O_RDWR);
+	if(fd == -1)
+		error_handle();
+	if(write(fd, pid, sizeof(pid)) == -1)
+		error_handle();
+	//printf("wrote: '%s' to fifo\n", pid);
+	close(fd);
+}
+
+int init_private_fifo(char *SERVER_DIR, char pid[20]) {
+	char *fifo = strcat(SERVER_DIR, pid);
+	if(access(fifo, F_OK) && mkfifo(fifo, 0666) == -1)
+		error_handle();
+	return open(fifo, O_RDWR);
+}
 
 int main(int argc, char *argv[]) {
 	if(argc != 3) {
@@ -14,13 +37,11 @@ int main(int argc, char *argv[]) {
 		exit(EXIT_FAILURE);
 	}
 
-	char *fifo = strcat(argv[1], KNOWN_FIFO);
-	int fd = open(fifo, O_RDWR);
-	if(fd == -1) {
-		perror("ERROR"); exit(EXIT_FAILURE);
-	}
 	char pid[20];
 	sprintf(pid, "%ld", (long)getpid());
-	write(fd, pid, sizeof(pid));
-	printf("wrote: '%s' to fifo\n", pid);
+
+	int send_images = init_private_fifo(argv[1], pid);
+	if(send_images == -1)
+		error_handle();
+	notify_daemon(argv[1], pid);
 }
--- submit/prj3-sol/knnd/main.c
+++ submit/prj3-sol/knnd/main.c
@@ -1,67 +1,101 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-#define KNOWN_FIFO "notify.fifo"
+#include <knn_ocr.h>
 
-void worker(void) {
+#define KNOWN_FIFO "notify"
 
+void error_handle(void) {
+	perror("Error");
+	exit(EXIT_FAILURE);
+}
+
+int open_worker_fifo(char pid[20]) {
+	return access(pid, F_OK) ? open(pid, O_RDONLY) : error_handle();
+}
+
+void do_work(char pid[20]) {
+	int fd = open_worker_fifo(pid);
+	if(fd == -1)
+		error_handle();
+	for(;;) {
+		if(read(fd, buf, sizeof(buf)) == -1)
+			error_handle();
+
+		if(buf[0] == '\0') break;
+	} 
 	exit(EXIT_SUCCESS);
 }
 
-void service(int argc, char *argv[]) {
-	if(access(KNOWN_FIFO, F_OK)) {
-		if(mkfifo(KNOWN_FIFO, 0666) == -1) {
-			perror("ERROR"); exit(EXIT_FAILURE);
+void setup_worker(char pid[20]) {
+	long rc = fork();
+	if(rc) {
+		exit(EXIT_SUCCESS);
+	} else if(!rc) {
+		if(rc = fork()) { 
+			exit(EXIT_SUCCESS);
+		} else if(!rc) {
+			do_work(pid);
+		} else {
+			error_handle();
 		}
+	} else { 
+		error_handle();
 	}
+}
+
+void service(int argc, char *argv[]) {
+	if(access(KNOWN_FIFO, F_OK) && mkfifo(KNOWN_FIFO, 0666) == -1)
+		error_handle();
 	int fd = open(KNOWN_FIFO, O_RDWR);
-	if(fd == -1) {
-		perror("ERROR"); exit(EXIT_FAILURE);
-	}
+	if(fd == -1)
+		error_handle();
 	char buf[20];
 	for(;;) {
-		read(fd, buf, sizeof(buf));
-		printf("buf:%s\n", buf);
+		if(read(fd, buf, sizeof(buf)) == -1)
+			error_handle();
+		//printf("buf:%s\n", buf);
+		setup_worker(buf);
+		memset(buf, 0, sizeof(buf));
 	}
 	exit(EXIT_FAILURE);
 }
+
 void setup_daemon(int argc, char *argv[]) {
-	if(setsid() == -1) {
-		perror("ERROR"); exit(EXIT_FAILURE);
-	}
+	if(setsid() == -1)
+		error_handle();
 	long rc = fork();
 	if(rc) {
 		printf("Daemon with PID %ld created.\n", rc);
 		exit(EXIT_SUCCESS);
 	} else if(!rc) {
-		if(chdir(argv[1]) == -1) {
-			perror("ERROR"); exit(EXIT_FAILURE);
-		}
+		if(chdir(argv[1]) == -1)
+			error_handle();
 		umask(0);
 		service(argc, argv);
 	} else {
-		perror("ERROR"); exit(EXIT_FAILURE);
+		error_handle();
 	}
 	exit(EXIT_FAILURE);
 }
+
 int main(int argc, char *argv[]) {
 	if(argc != 3) {
 		printf("Usage: ./knnd SERVER_DIR DATA_DIR [K]\n");
 		exit(EXIT_FAILURE);
 	}
-	//service(argc, argv);
 	long rc = fork();
 	if(rc) {
-		//printf("Daemon with PID %ld created.\n", rc);
 		exit(EXIT_SUCCESS);
 	} else if(!rc) {
 		setup_daemon(argc, argv);
 	} else {
-		perror("ERROR"); exit(EXIT_FAILURE);
+		error_handle();
 	}
 	exit(EXIT_FAILURE);
 }
